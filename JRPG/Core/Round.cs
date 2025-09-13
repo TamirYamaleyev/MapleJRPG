f6fe71621b845b51168f43f598426a241c43594f
Tamir Yamaleyev <tamirka8@gmail.com>
2025-09-14 02:13:59 +0300

    turned Round into a singleton to keep track of it accross the entire project

---
+++ JRPG/Core/Round.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JRPG.Core
+{
+    internal class Round
+    {
+        private Round() { }
+        private static readonly Round _instance = new Round();
+        public static Round Instance => _instance;
+        public int CurrentRound { get; set; } = 1;
+    }
+}
