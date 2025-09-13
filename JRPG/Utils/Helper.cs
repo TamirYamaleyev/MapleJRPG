7a20d3bfecf5bd5e4ffd471941503a91031e7d06
Tamir Yamaleyev <tamirka8@gmail.com>
2025-09-14 02:15:39 +0300

    Added ChoiceSelection helper method for selecting a choice from a menu using the arrow keys instead of normal console value input

--- JRPG/Utils/Helper.cs
+++ JRPG/Utils/Helper.cs
@@ -15,5 +15,53 @@ namespace JRPG.Utils
             Random random = new Random();
             return random.Next((int)(value * minRangeMultiplier), (int)(value * maxRangeMultiplier));
         }
+
+        public static int ChoiceSelection(string prompt, string[] options, string[]? tooltips = null)
+        {
+            int selectedIndex = 0;
+            ConsoleKey key;
+
+            int menuTop = Console.WindowHeight - options.Length - 5;
+
+            do
+            {
+                Console.SetCursorPosition(0, menuTop);
+                Console.WriteLine(prompt.PadRight(Console.WindowWidth));
+                Console.WriteLine(new string(' ', Console.WindowWidth));
+
+                // Display menu options
+                for (int i = 0; i < options.Length; i++)
+                {
+                    string line = (i == selectedIndex ? $"> {options[i]}" : $"  {options[i]}").PadRight(Console.WindowWidth);
+                    Console.ForegroundColor = i == selectedIndex ? ConsoleColor.Green : Console.ForegroundColor;
+                    Console.WriteLine(line);
+                    Console.ResetColor();
+                }
+
+                // Display tooltip if available, padded to console width
+                string tooltip = "";
+                if (tooltips != null && selectedIndex < tooltips.Length && !string.IsNullOrWhiteSpace(tooltips[selectedIndex]))
+                    tooltip = tooltips[selectedIndex];
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(tooltip.PadRight(Console.WindowWidth));
+                Console.ResetColor();
+
+                key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.UpArrow)
+                    selectedIndex = (selectedIndex - 1 + options.Length) % options.Length;
+                else if (key == ConsoleKey.DownArrow)
+                    selectedIndex = (selectedIndex + 1) % options.Length;
+
+                // Move cursor back to top of menu for next redraw
+                Console.SetCursorPosition(0, menuTop);
+
+            } while (key != ConsoleKey.Enter);
+
+            return selectedIndex;
+        }
+
+
     }
 }
