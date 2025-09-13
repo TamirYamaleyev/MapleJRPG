452e23095e7264b57e161f6b764e77e519177948
Tamir Yamaleyev <tamirka8@gmail.com>
2025-09-14 02:23:30 +0300

    rewrote Program.cs to work with the new Game class + Deleted States folder

--- JRPG/Program.cs
+++ JRPG/Program.cs
@@ -1,12 +1,7 @@
-﻿using JRPG.Core;
+﻿using JRPG;
+using JRPG.Core;
 using NAudio.Wave;
 using System.Media;
 
-Player p1 = new Player();
-Enemy sheep = new Enemy();
-
-Console.WriteLine(sheep.CurrentHealth);
-p1.NormalAttack(sheep);
-Console.WriteLine(sheep.CurrentHealth);
-p1.NormalAttack(sheep);
-Console.WriteLine(sheep.CurrentHealth);
+Game game = new Game();
+game.Run();
